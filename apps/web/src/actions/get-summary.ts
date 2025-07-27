23fab9a5355e6ca866ad4fbd7f268eaa667548e5
0xTeo <mooon3356@gmail.com>
2025-07-28 00:06:27 +0900

    ci: add continuous intergrations for web and video worker services using GitHub Actions (#27)
    * ci: add continous integration for API using GitHub Actions
    
    * chore(api): GitHub Actions CI test
    
    * ci: add continous integration for Web using GitHub Actions
    
    * chore(web): bumb version to 1.0.0
    
    * fix(web): build error
    
    * ci: add continous integration for Video Worker using GitHub Actions
    
    * chore(video-worker): bump version to 1.0.0

--- apps/web/src/actions/get-summary.ts
+++ apps/web/src/actions/get-summary.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { fetchWithError } from "@/lib/fetch";
 import { SummaryDTO, summarySchema } from "@polinote/schemas";
-import { Language } from "@polinote/entities";
+import { Language } from "@polinote/schemas";
 
 export async function getSummary(videoId: string, language: Language): Promise<SummaryDTO> {
   const response = await fetchWithError<z.infer<typeof summarySchema>>(
